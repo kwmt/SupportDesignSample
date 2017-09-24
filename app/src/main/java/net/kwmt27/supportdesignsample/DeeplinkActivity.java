a7638b5768da32b04bb80632bc1ed08e51bccd79
Yasutaka Kawamoto <kwmt27@gmail.com>
2017-09-24 17:16:28 +0900

    DeepLink Bottom Sheet

---
+++ app/src/main/java/net/kwmt27/supportdesignsample/DeeplinkActivity.java
@@ -0,0 +1,13 @@
+package net.kwmt27.supportdesignsample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DeeplinkActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_deeplink);
+    }
+}
