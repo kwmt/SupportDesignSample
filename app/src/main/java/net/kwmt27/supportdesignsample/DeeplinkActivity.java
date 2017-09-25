1fd585599542908e294d9d772f14b33a1b45093e
Yasutaka Kawamoto <kwmt27@gmail.com>
2017-09-25 09:33:18 +0900

    deeplink経由で表示(できない)

--- app/src/main/java/net/kwmt27/supportdesignsample/DeeplinkActivity.java
+++ app/src/main/java/net/kwmt27/supportdesignsample/DeeplinkActivity.java
@@ -1,13 +1,64 @@
 package net.kwmt27.supportdesignsample;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.res.TypedArray;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomSheetBehavior;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class DeeplinkActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setStatusBarDim(true);
         setContentView(R.layout.activity_deeplink);
+
+        findViewById(R.id.touch_outside).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        BottomSheetBehavior.from(findViewById(R.id.bottom_sheet))
+                .setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+                    @Override
+                    public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                        switch (newState) {
+                            case BottomSheetBehavior.STATE_HIDDEN:
+                                finish();
+                                break;
+                            case BottomSheetBehavior.STATE_EXPANDED:
+                                setStatusBarDim(false);
+                                break;
+                            default:
+                                setStatusBarDim(true);
+                                break;
+                        }
+                    }
+
+                    @Override
+                    public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+
+                    }
+                });
+    }
+    private void setStatusBarDim(boolean dim) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setStatusBarColor(dim ? Color.TRANSPARENT :
+                    ContextCompat.getColor(this, getThemedResId(R.attr.colorPrimaryDark)));
+        }
+    }
+
+    private int getThemedResId(int attr) {
+        TypedArray a = getTheme().obtainStyledAttributes(new int[]{attr});
+        int resId = a.getResourceId(0, 0);
+        a.recycle();
+        return resId;
     }
 }
